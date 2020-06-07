5715d2b26c512a754b8074a4c4b91bebb7e5b856
Kotovich Sergey <48524741+kotovichsergey@users.noreply.github.com>
2020-06-08 02:21:48 +0300

    fix: getWeather

--- app/Controllers/WeatherController.php
+++ app/Controllers/WeatherController.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 class WeatherController extends Controller {
     public function getWeather($request, $response) {
         $weather = Weather::whereDate('dt', Carbon::today()->format('Y-m-d'))->orderByDesc('dt')->first();
-        if ($weather->count()) {            
+        if ($weather) {            
             return $this->response->withJson($weather);
         }
         return $this->response->withJson(['msg' => 'На сегодня данных нет.']);        
